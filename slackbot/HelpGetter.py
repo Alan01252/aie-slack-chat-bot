39510bf158a668a1168b3601d018239aded7f04f
Hollis <alan.hollis@capgemini.com>
2017-05-25 16:59:18 +0100

    Add help message

--- slackbot/HelpGetter.py
+++ slackbot/HelpGetter.py
@@ -12,6 +12,7 @@ class HelpGetter(object):
         return "Welcome to the AIE Bot " \
                "\n\n Current commands: \n\n " \
                "help : Returns this message \n\n " \
+               "people : Returns the people who have checked in this week and where they checked in \n\n " \
                "get_registered : Returns a link to get a spreadsheet of users \n\n" \
                "register : ChecksIn/Out of a location \n\n" \
                "        Usage: /aie register [UserName] [Location] e.g /aie register AlanHollis Working From Home"
